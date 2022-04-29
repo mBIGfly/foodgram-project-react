f3a2de422afac40ee8adfd39104bd332a443b409
mBIGfly <84184582+mBIGfly@users.noreply.github.com>
2022-04-29 16:15:25 +0300

    Foodgram_project rev.4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -3,8 +3,7 @@ import sys
 
 from dotenv import load_dotenv
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 load_dotenv()
 
