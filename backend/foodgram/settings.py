32e50dd65fbe5217b87657aa5127f5c1c927b78e
mBIGfly <84184582+mBIGfly@users.noreply.github.com>
2022-04-29 17:29:33 +0300

    Foodgram_project rev.4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY',
-                       default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
+                       default='very-secret-key@#RFesf')
 
 
 DEBUG = int(os.environ.get('DEBUG', '0'))
