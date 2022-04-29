c977df5f9f69b83af4b6090c7aaf7920261eecfb
mBIGfly <84184582+mBIGfly@users.noreply.github.com>
2022-04-29 17:06:53 +0300

    Foodgram_project rev.4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -79,9 +79,9 @@ else:
         'default': {
             'ENGINE': os.getenv(
                 'DB_ENGINE', default='django.db.backends.postgresql'),
-            'NAME': os.getenv('DB_NAME', default='db'),
-            'USER': os.getenv('POSTGRES_USER', default='dbuser'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='dbpass'),
+            'NAME': os.getenv('DB_NAME', default='postgres'),
+            'USER': os.getenv('POSTGRES_USER', default='postgres'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
             'HOST': os.getenv('DB_HOST', default='db'),
             'PORT': os.getenv('DB_PORT', default=5432)
         }
