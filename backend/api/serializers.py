eaf5cd937d414713182a4ddc6b31db5ef4498910
mBIGfly <84184582+mBIGfly@users.noreply.github.com>
2022-05-07 12:14:31 +0300

    Foodgram_project rev.6

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -154,26 +154,23 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
     def get_is_favorited(self, obj):
         return self.__is_recipe(obj, Favorite)
 
-    def ingredient_in_ingredients(self, recipe, ingredients):
-        for ingredient in ingredients:
-            IngredientRecipeRelation.objects.create(
-                recipe, ingredient=ingredient['ingredient'],
-                amount=ingredient['amount']
-            ).save()
-
     @transaction.atomic
     def create(self, validated_data):
         ingredients = validated_data.pop('ingredients')
         tags = validated_data.pop('tags')
 
         obj = Recipe.objects.create(**validated_data)
+        obj.save()
 
         obj.tags.set(tags)
 
-        recipe = obj
-        self.ingredient_in_ingredients(recipe, ingredients)
+        for ingredient in ingredients:
+            IngredientRecipeRelation.objects.create(
+                recipe=obj, ingredient=ingredient['ingredient'],
+                amount=ingredient['amount']
+            ).save()
 
-        return Recipe.objects.create(**validated_data)
+        return obj
 
     def validate(self, data):
         keys = ('ingredients', 'tags', 'text', 'name', 'cooking_time')
@@ -187,6 +184,22 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
         if errors:
             raise serializers.ValidationError(errors, code='field_error')
 
+        ingredients = self.data.get('ingredients')
+        ingredients_set = set()
+        for ingredient in ingredients:
+            if int(ingredient.get('amount')) <= 0:
+                raise serializers.ValidationError(
+                    ('Убедитесь, что значение количества '
+                     'ингредиента больше 0')
+                )
+            ingredient_id = ingredient.get('id')
+            if ingredient_id in ingredients_set:
+                raise serializers.ValidationError(
+                    'Ингредиент в рецепте не должен повторяться.'
+                )
+            ingredients_set.add(ingredient_id)
+        data['ingredients'] = ingredients
+
         return data
 
     @transaction.atomic
@@ -194,17 +207,20 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
         ingredients = validated_data.pop('ingredients')
         tags = validated_data.pop('tags')
 
-        super().update(instance, validated_data)
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
 
         instance.tags.set(tags)
         instance.image = validated_data.get('image', instance.image)
 
         instance.ingredients.clear()
+        for ingredient in ingredients:
+            IngredientRecipeRelation.objects.create(
+                recipe=instance, ingredient=ingredient['ingredient'],
+                amount=ingredient['amount']
+            ).save()
 
-        recipe = instance
-        self.ingredient_in_ingredients(recipe, ingredients)
-
-        return super().update(recipe, validated_data)
+        return super().update(instance, validated_data)
 
     def to_representation(self, instance):
         self.fields.pop('ingredients')
