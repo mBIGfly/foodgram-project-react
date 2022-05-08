40c7362a8812afcc474741ec59ee55140cda438b
mBIGfly <84184582+mBIGfly@users.noreply.github.com>
2022-05-08 12:11:51 +0300

    Foodgram_project rev.6

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -182,24 +182,6 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
 
         return data
 
-    def validate_data(self, data):
-        ingredients = data.get("ingredients")
-        ingredients_set = set()
-        for ingredient in ingredients:
-            if int(ingredient.get("amount")) <= 0:
-                raise serializers.ValidationError(
-                    ("Убедитесь, что значение количества "
-                     "ингредиента больше 0")
-                )
-            id = ingredient.get("id")
-            if id in ingredients_set:
-                raise serializers.ValidationError(
-                    "Ингредиент в рецепте не должен повторяться."
-                )
-            ingredients_set.add(id)
-        data["ingredients"] = ingredients
-        return data
-
     @transaction.atomic
     def update(self, instance, validated_data):
         ingredients = validated_data.pop('ingredients')
@@ -218,6 +200,24 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
             ).save()
 
         return super().update(instance, validated_data)
+
+    def validate_data(self, data):
+        ingredients = data.get("ingredients")
+        ingredients_set = set()
+        for ingredient in ingredients:
+            if int(ingredient.get("amount")) <= 0:
+                raise serializers.ValidationError(
+                    ("Убедитесь, что значение количества "
+                     "ингредиента больше 0")
+                )
+            id = ingredient.get("id")
+            if id in ingredients_set:
+                raise serializers.ValidationError(
+                    "Ингредиент в рецепте не должен повторяться."
+                )
+            ingredients_set.add(id)
+        data["ingredients"] = ingredients
+        return data
 
     def to_representation(self, instance):
         self.fields.pop('ingredients')
