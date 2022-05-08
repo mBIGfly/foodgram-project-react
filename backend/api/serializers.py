4df36a254c26c6589ee0bde4aee8ace8845a16e5
mBIGfly <84184582+mBIGfly@users.noreply.github.com>
2022-05-08 11:59:36 +0300

    Foodgram_project rev.6

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -172,32 +172,11 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
         return obj
 
     def validate(self, data):
-        # keys = ('ingredients', 'tags', 'text', 'name', 'cooking_time')
-
-        # errors = {}
-
-        # for key in keys:
-        #     if key not in data:
-        #         errors.update({key: 'Обязательное поле'})
-
-        # if errors:
-        #     raise serializers.ValidationError(errors, code='field_error')
-
-        # return data
         keys = ('ingredients', 'tags', 'text', 'name', 'cooking_time')
         errors = {}
-        ingredients_set = data.get('ingredients')
-
         for key in keys:
             if key not in data:
                 errors.update({key: 'Обязательное поле'})
-
-        for ingredient in ingredients_set:
-            if int(ingredient.get('amount')) <= 0:
-                errors.update(
-                    ('Убедитесь, что значение количества '
-                     'ингредиента больше 0'))
-
         if errors:
             raise serializers.ValidationError(errors, code='field_error')
 
