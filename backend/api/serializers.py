ab9929b3d58b35b988966cd203e5c1415cf2f9ea
mBIGfly <84184582+mBIGfly@users.noreply.github.com>
2022-05-08 14:47:35 +0300

    Foodgram_project rev.6

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -189,11 +189,12 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
                      'ингредиента больше 0')
                 )
             ingredient_id = ingredient.get('id')
-            if ingredient_id in ingredients_set:
-                raise serializers.ValidationError(
-                    'Ингредиент в рецепте не должен повторяться.'
-                )
-            ingredients_set.add(ingredient_id)
+            if ingredient_id not in ingredients_set:
+                ingredients_set.add(ingredient_id)
+            raise serializers.ValidationError(
+                'Ингредиент в рецепте не должен повторяться.'
+            )
+
         data['ingredients'] = ingredients
 
         return data
