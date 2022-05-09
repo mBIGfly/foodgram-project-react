f2b715ff2782e08b1602ea4dd2de9b98910e49f3
mBIGfly <84184582+mBIGfly@users.noreply.github.com>
2022-05-10 00:59:27 +0300

    Foodgram_project rev.6

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,6 @@ import djoser.serializers
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.shortcuts import get_object_or_404
-from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework.settings import api_settings
 
