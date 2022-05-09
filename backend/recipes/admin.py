b861be09c68383a643d59e03917c4e774ce143e5
mBIGfly <84184582+mBIGfly@users.noreply.github.com>
2022-05-09 20:50:52 +0300

    Foodgram_project rev.6

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -30,7 +30,7 @@ class IngredientRecipeRelationAdminInline(admin.TabularInline):
 
 
 class RecipeAdmin(AdminImageMixin, admin.ModelAdmin):
-    list_display = ('name', 'author', 'tags_name', 'favorite_count')
+    list_display = ('name', 'author', 'favorite_count')
     search_fields = ('name', 'author__username', 'author__last_name',
                      'tags__name', 'tags__slug', 'author__first_name')
     inlines = (IngredientRecipeRelationAdminInline,)
